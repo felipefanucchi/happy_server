9e6647f51a491f45408462f46bb94c6c91f59756
Felipe Fanucchi <felipefanucchi2010@hotmail.com>
2020-10-18 09:20:00 -0300

    Habilita cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './database/connection';
@@ -10,6 +11,7 @@ import errorHandler from './errors/handler';
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(errorHandler);
